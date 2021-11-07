27f544cbd176b544ecd038f177673d180472d41f
Zdobdol <zdobdol@gmail.com>
2021-11-07 20:12:09 +0100

    CRUD - CREATE READ UPDATE DELETE

--- filmyweb/urls.py
+++ filmyweb/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from filmyweb.views import all_films
+from filmyweb.views import all_films, new_films, edit_film, delete_film
 
 urlpatterns = [
-    path('all/', all_films),
+    path('all/', all_films, name='all_films'),
+    path('new', new_films, name='new_film'),
+    path('edit/<int:id>/', edit_film, name="edit_film"),
+    path('delete/<int:id>/', delete_film, name='delete_film'),
+
 ]
--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Film
+from .forms import FilmForm
 
 
 def all_films(request):
     all = Film.objects.all()
     return render(request,'filmy.html',{ 'films': all })
+
+def new_films(request):
+    form = FilmForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect(all_films)
+
+    return render(request, 'film_form.html', {'form': form})
+
+def edit_film(request, id):
+    film = get_object_or_404(Film, pk=id)
+    form = FilmForm(request.POST or None, request.FILES or None, instance=film)
+
+    if form.is_valid():
+        form.save()
+        return redirect(all_films)
+
+    return render(request, 'film_form.html', {'form': form})
+
+def delete_film(request, id):
+    film = get_object_or_404(Film, pk=id)
+
+    if request.method == 'POST':
+        film.delete()
+        return redirect(all_films)
+
+    return render(request, 'confirm.html', {'film': film})
+
+
